f19462f7aee16a9ff08cdab5e6aa94e3386316d2
zachjrt <tho19091@byui.edu>
2022-01-11 17:43:42 -0700

    Commit

--- tictac.py
+++ tictac.py
@@ -1,13 +1,16 @@
 """
 Author: Zach Thompson
 """
+gamePlay = True;
 
 def main():
     currentPlayer = "none"
+    nextPlayer(currentPlayer)
     board = makeBoard()
-    gamePlay = true;
     while (gamePlay):
         showBoard(board)
+        chooseSpot(currentPlayer, board)
+        currentPlayer = nextPlayer(currentPlayer)
     
 def makeBoard():
     board = []
@@ -15,16 +18,17 @@ def makeBoard():
         board.append(spot + 1)
     return board
 
-def showBoard():   
+def showBoard(board):   
     print(f"{board[0]}|{board[1]}|{board[2]}")
     print('-----')
     print(f"{board[3]}|{board[4]}|{board[5]}")
     print('-----')
     print(f"{board[6]}|{board[7]}|{board[8]}")
     
-def chooseSpot():
+def chooseSpot(currentPlayer, board):
     spot = int(input(f"{currentPlayer}'s turn to choose a square [1-9]: "))
     board[spot - 1] = currentPlayer
+    checkGame(board)
 
 def nextPlayer(currentPlayer):
     if currentPlayer == "none" or currentPlayer == "O":
@@ -32,7 +36,15 @@ def nextPlayer(currentPlayer):
     elif currentPlayer == "X":
         currentPlayer == "O"
     return currentPlayer
-    
+
+def checkGame(board):
+    if (board[0] == board[1] == board[2] or board[3] == board[4] == board[5] or
+        board[6] == board[7] == board[8] or board[0] == board[3] == board[6] or
+        board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or
+        board[0] == board[4] == board[8] or board[2] == board[4] == board[6]):
+        gamePlay = False;
+    else:
+        gamePlay = True;
 
 if __name__ == "__main__":
     main()
